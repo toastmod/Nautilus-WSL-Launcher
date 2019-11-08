a6722067b575559fcddcc350acdef4ccff85bc84
Andrew Toast <toastymod@gmail.com>
2019-11-08 11:20:57 -0700

    Add files via upload

---
+++ XTerm.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+QPROCESS vcxsrv.exe >NUL
+
+IF %ERRORLEVEL%==0 ((
+	ECHO "===RUNNING==="
+	ubuntu run "export DISPLAY=:0 && xterm"
+) ELSE (
+	ECHO "===OPENING SERVER==="
+	start config.xlaunch -renderer
+	ECHO "===RUNNING==="
+	ubuntu run "export DISPLAY=:0 && xterm"
+)
+
+pause
